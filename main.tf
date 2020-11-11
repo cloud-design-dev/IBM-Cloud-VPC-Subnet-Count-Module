eb95c80aa547fba6245a49f460a33f393117b96b
greyhoundforty <ryantiffany@fastmail.com>
2020-11-11 07:41:11 -0600

    Fixing address count bug
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource ibm_is_subnet subnet {
   name                     = "${var.name}-${var.zone}-subnet"
   vpc                      = var.vpc_id
   zone                     = var.zone
-  ipv4_cidr_block          = local.subnet_cidr
   total_ipv4_address_count = local.address_count
   network_acl              = local.network_acl
   public_gateway           = local.public_gateway
@@ -10,7 +9,6 @@ resource ibm_is_subnet subnet {
 }
 
 locals {
-  subnet_cidr    = var.subnet_cidr != "" ? var.subnet_cidr : ""
   address_count  = var.address_count != "" ? var.address_count : "256"
   network_acl    = var.network_acl != "" ? var.network_acl : ""
   public_gateway = var.public_gateway != "" ? var.public_gateway : ""
