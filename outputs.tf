4148e40457725f95b6f30370d541fe5a67353ffc
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2020-11-11 12:27:37 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,7 @@
 output id {
-  value = ibm_is_subnet.subnet.*.id
-}
-
-output available_ipv4_address_count {
-  value = ibm_is_subnet.subnet.*.available_ipv4_address_count
+  value = ibm_is_subnet.subnet.id
 }
 
 output ipv4_cidr_block {
-  value = ibm_is_subnet.subnet.*.ipv4_cidr_block
+  value = ibm_is_subnet.subnet.ipv4_cidr_block
 }
