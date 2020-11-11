ac012008c72d98a00405afb55c95501b3322c4d4
greyhoundforty <ryantiffany@fastmail.com>
2020-11-11 12:34:46 -0600

    Set default for subnet size
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
 variable resource_group {}
 variable name {}
-variable address_count {}
+variable address_count {
+  default = "256"
+}
 variable network_acl {}
 variable public_gateway {}
 variable vpc_id {}
