8b2ef312474e596a128f6a83520bba0255450305
greyhoundforty <ryantiffany@fastmail.com>
2020-11-11 12:26:09 -0600

    Removing count from individual modules
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 resource ibm_is_subnet subnet {
-  count = length(var.zone)
-  name                     = "${var.name}-${var.zone}-${count.index +1}-subnet"
+  name                     = "${var.name}-${var.zone}-subnet"
   vpc                      = var.vpc_id
-  zone                     = var.zone[count.index]
+  zone                     = var.zone
   total_ipv4_address_count = local.address_count
   network_acl              = local.network_acl
   public_gateway           = local.public_gateway
@@ -12,5 +11,5 @@ resource ibm_is_subnet subnet {
 locals {
   address_count  = var.address_count != "" ? var.address_count : "256"
   network_acl    = var.network_acl != "" ? var.network_acl : ""
-  public_gateway = var.public_gateway != "" ? var.public_gateway : ""
+  public_gateway = var.public_gateway != "" ? var.public_gateway : null
 }
